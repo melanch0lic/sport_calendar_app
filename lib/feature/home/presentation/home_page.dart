4c4e13e151d4964f619852a321b0ab2537379baf
melanch0lic <kudza76@mail.ru>
2024-11-25 22:12:42 +0300

    pull to refresh and home scope refactor

--- lib/feature/home/presentation/home_page.dart
+++ lib/feature/home/presentation/home_page.dart
@@ -25,133 +25,144 @@ class HomePage extends StatelessWidget {
           Container(
             color: const Color.fromRGBO(29, 31, 36, 1),
           ),
-          CustomScrollView(
-            slivers: [
-              const HomeAppBar(),
-              SliverToBoxAdapter(
-                child: Column(
-                  children: [
-                    ClipRRect(
-                      borderRadius: const BorderRadius.only(
-                        topLeft: Radius.circular(24),
-                        topRight: Radius.circular(24),
-                      ),
-                      child: Container(
-                        color: Colors.white,
-                        child: Column(
-                          // mainAxisSize: MainAxisSize.min,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 36),
-                              child: Row(
-                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Text(
-                                    'Активные фильтры соревнований',
-                                    style: CommonTextStyles().title2,
-                                  ),
-                                  const IconWithNotifyCircle(icon: 'assets/icons/filter.svg')
-                                ],
-                              ),
+          SafeArea(
+            child: Builder(builder: (context) {
+              return RefreshIndicator(
+                displacement: MediaQuery.sizeOf(context).height * 0.1,
+                onRefresh: () async {
+                  await Future.delayed(const Duration(milliseconds: 500));
+                  HomeScope.of(context).loadEvents();
+                },
+                child: CustomScrollView(
+                  slivers: [
+                    const HomeAppBar(),
+                    SliverToBoxAdapter(
+                      child: Column(
+                        children: [
+                          ClipRRect(
+                            borderRadius: const BorderRadius.only(
+                              topLeft: Radius.circular(24),
+                              topRight: Radius.circular(24),
                             ),
-                            Padding(
-                              padding: const EdgeInsets.symmetric(horizontal: 16),
-                              child: Row(
-                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Text(
-                                    'Ближайшие состязания',
-                                    style: CommonTextStyles()
-                                        .largeTitle
-                                        .copyWith(color: const Color.fromRGBO(29, 31, 36, 1)),
-                                  ),
-                                  SvgPicture.asset('assets/icons/more.svg')
-                                ],
-                              ),
-                            ),
-                            const SizedBox(height: 24),
-                            const AutoScrollingListView(),
-                            const SizedBox(height: 36),
-                            Padding(
-                              padding: const EdgeInsets.symmetric(horizontal: 16),
+                            child: Container(
+                              color: Colors.white,
                               child: Column(
+                                // mainAxisSize: MainAxisSize.min,
                                 crossAxisAlignment: CrossAxisAlignment.start,
                                 children: [
-                                  Text(
-                                    'Соревнования для вас',
-                                    style: CommonTextStyles()
-                                        .largeTitle
-                                        .copyWith(color: const Color.fromRGBO(29, 31, 36, 1)),
-                                  ),
-                                  const SizedBox(height: 16),
-                                  BlocBuilder<EventBloc, EventState>(builder: (context, state) {
-                                    if (state is EventLoading) {
-                                      return Shimmer.fromColors(
-                                        baseColor: Colors.grey.shade300,
-                                        highlightColor: Colors.grey.shade100,
-                                        enabled: true,
-                                        child: const SingleChildScrollView(
-                                          padding: EdgeInsets.only(bottom: 36, top: 16),
-                                          physics: NeverScrollableScrollPhysics(),
-                                          child: Column(
-                                            crossAxisAlignment: CrossAxisAlignment.start,
-                                            mainAxisSize: MainAxisSize.max,
-                                            children: [
-                                              TitlePlaceholder(width: double.infinity),
-                                              SizedBox(height: 16.0),
-                                              ContentPlaceholder(lineType: ContentLineType.threeLines),
-                                              SizedBox(height: 16.0),
-                                              TitlePlaceholder(width: double.infinity),
-                                              SizedBox(height: 16.0),
-                                              ContentPlaceholder(lineType: ContentLineType.threeLines),
-                                              SizedBox(height: 16.0),
-                                              TitlePlaceholder(width: double.infinity),
-                                              SizedBox(height: 16.0),
-                                              ContentPlaceholder(lineType: ContentLineType.threeLines),
-                                            ],
-                                          ),
+                                  Padding(
+                                    padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 36),
+                                    child: Row(
+                                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        Text(
+                                          'Активные фильтры соревнований',
+                                          style: CommonTextStyles().title2,
                                         ),
-                                      );
-                                    } else if (state is EventLoaded) {
-                                      Map<String, List<EventData>> groupedEvents = {};
-                                      for (var event in state.events) {
-                                        if (groupedEvents[event.sportTypeId] == null) {
-                                          groupedEvents[event.sportTypeId] = [];
-                                        }
-                                        groupedEvents[event.sportTypeId]?.add(event);
-                                      }
+                                        const IconWithNotifyCircle(icon: 'assets/icons/filter.svg')
+                                      ],
+                                    ),
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.symmetric(horizontal: 16),
+                                    child: Row(
+                                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        Text(
+                                          'Ближайшие состязания',
+                                          style: CommonTextStyles()
+                                              .largeTitle
+                                              .copyWith(color: const Color.fromRGBO(29, 31, 36, 1)),
+                                        ),
+                                        SvgPicture.asset('assets/icons/more.svg')
+                                      ],
+                                    ),
+                                  ),
+                                  const SizedBox(height: 24),
+                                  const AutoScrollingListView(),
+                                  const SizedBox(height: 36),
+                                  Padding(
+                                    padding: const EdgeInsets.symmetric(horizontal: 16),
+                                    child: Column(
+                                      crossAxisAlignment: CrossAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                          'Соревнования для вас',
+                                          style: CommonTextStyles()
+                                              .largeTitle
+                                              .copyWith(color: const Color.fromRGBO(29, 31, 36, 1)),
+                                        ),
+                                        const SizedBox(height: 16),
+                                        BlocBuilder<EventBloc, EventState>(builder: (context, state) {
+                                          if (state is EventLoading) {
+                                            return Shimmer.fromColors(
+                                              baseColor: Colors.grey.shade300,
+                                              highlightColor: Colors.grey.shade100,
+                                              enabled: true,
+                                              child: const SingleChildScrollView(
+                                                padding: EdgeInsets.only(bottom: 36, top: 16),
+                                                physics: NeverScrollableScrollPhysics(),
+                                                child: Column(
+                                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                                  mainAxisSize: MainAxisSize.max,
+                                                  children: [
+                                                    TitlePlaceholder(width: double.infinity),
+                                                    SizedBox(height: 16.0),
+                                                    ContentPlaceholder(lineType: ContentLineType.threeLines),
+                                                    SizedBox(height: 16.0),
+                                                    TitlePlaceholder(width: double.infinity),
+                                                    SizedBox(height: 16.0),
+                                                    ContentPlaceholder(lineType: ContentLineType.threeLines),
+                                                    SizedBox(height: 16.0),
+                                                    TitlePlaceholder(width: double.infinity),
+                                                    SizedBox(height: 16.0),
+                                                    ContentPlaceholder(lineType: ContentLineType.threeLines),
+                                                  ],
+                                                ),
+                                              ),
+                                            );
+                                          } else if (state is EventLoaded) {
+                                            Map<String, List<EventData>> groupedEvents = {};
+                                            for (var event in state.events) {
+                                              if (groupedEvents[event.sportTypeId] == null) {
+                                                groupedEvents[event.sportTypeId] = [];
+                                              }
+                                              groupedEvents[event.sportTypeId]?.add(event);
+                                            }
 
-                                      return ColumnBuilder.separator(
-                                        // physics: const PageScrollPhysics(),
-                                        itemCount: groupedEvents.keys.length,
-                                        separatorBuilder: (context, index) => const SizedBox(height: 16),
-                                        itemBuilder: (context, index) {
-                                          String sportTypeId = groupedEvents.keys.elementAt(index);
-                                          List<EventData> events = groupedEvents[sportTypeId]!;
+                                            return ColumnBuilder.separator(
+                                              // physics: const PageScrollPhysics(),
+                                              itemCount: groupedEvents.keys.length,
+                                              separatorBuilder: (context, index) => const SizedBox(height: 16),
+                                              itemBuilder: (context, index) {
+                                                String sportTypeId = groupedEvents.keys.elementAt(index);
+                                                List<EventData> events = groupedEvents[sportTypeId]!;
 
-                                          return PresentationContainerDropdown(
-                                            sportTypeId: sportTypeId,
-                                            events: events,
-                                          );
-                                        },
-                                      );
-                                    } else if (state is EventError) {
-                                      return Center(child: Text(state.message));
-                                    }
-                                    return const Center(child: CircularProgressIndicator());
-                                  }),
+                                                return PresentationContainerDropdown(
+                                                  sportTypeId: sportTypeId,
+                                                  events: events,
+                                                );
+                                              },
+                                            );
+                                          } else if (state is EventError) {
+                                            return Center(child: Text(state.message));
+                                          }
+                                          return const Center(child: CircularProgressIndicator());
+                                        }),
+                                      ],
+                                    ),
+                                  )
                                 ],
                               ),
-                            )
-                          ],
-                        ),
+                            ),
+                          ),
+                        ],
                       ),
-                    ),
+                    )
                   ],
                 ),
-              )
-            ],
+              );
+            }),
           ),
         ],
       ),
--- lib/feature/home/presentation/home_scope.dart
+++ lib/feature/home/presentation/home_scope.dart
@@ -1,30 +1,50 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:sport_calendart_app/core/utils/extension/src/context_extension.dart';
 import 'package:sport_calendart_app/feature/home/bloc_event/event_bloc.dart';
 
 // import '../../localization_control/bloc/localization_control_bloc.dart';
 // import '../../localization_control/presentation/localization_scope.dart';
 import '../../../runner/dependency_scope.dart';
 import '../bloc/home_bloc.dart';
-import '../domain/repositories/event_repository.dart';
+
+abstract class HomeController {
+  void loadEvents();
+}
 
 @immutable
-class HomeScope extends StatelessWidget {
+class HomeScope extends StatefulWidget {
   final Widget child;
 
   const HomeScope({required this.child, super.key});
 
-  static void readOf(BuildContext context) {
-    // final locale = LocalizationScope.getLocaleCode(context, listen: false);
-    // context.read<HomeBloc>().add(HomeEvent.read(locale: locale));
+  static HomeController of(BuildContext context, {bool listen = true}) =>
+      context.inhOf<_HomeScopeInherited>(listen: listen).controller;
+
+  @override
+  State<HomeScope> createState() => _HomeScopeState();
+}
+
+class _HomeScopeState extends State<HomeScope> implements HomeController {
+  late final HomeBloc _homeBloc;
+  late final EventBloc _eventBloc;
+
+  @override
+  void initState() {
+    _homeBloc = DependenciesScope.of(context).resolve<HomeBloc>();
+    _eventBloc = DependenciesScope.of(context).resolve<EventBloc>();
+    super.initState();
   }
 
   @override
+  void loadEvents() => _eventBloc.add(LoadEvents());
+
+  @override
   Widget build(BuildContext context) => BlocProvider<HomeBloc>(
-        create: (context) => HomeBloc(),
+        create: (context) => _homeBloc,
         child: BlocProvider<EventBloc>(
-          create: (context) => EventBloc(DependenciesScope.of(context).resolve<EventRepository>())..add(LoadEvents()),
-          child: _LocaleListener(child: child),
+          create: (context) => _eventBloc..add(LoadEvents()),
+          child: _HomeScopeInherited(controller: this, child: _LocaleListener(child: widget.child)),
         ),
       );
 }
@@ -41,4 +61,12 @@ class _LocaleListener extends StatelessWidget {
     // child: child,
     // );
   }
+}
+
+class _HomeScopeInherited extends InheritedWidget {
+  final HomeController controller;
+  const _HomeScopeInherited({required this.controller, required super.child});
+
+  @override
+  bool updateShouldNotify(_HomeScopeInherited oldWidget) => false;
 }
